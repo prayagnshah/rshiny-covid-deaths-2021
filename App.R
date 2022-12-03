37cac8b157af5aac3cb41e2ea0d47340af883dbf
prayagnshah <34435248+prayagnshah@users.noreply.github.com>
2022-12-03 06:07:27 -0400

    Outline prepared for the application
    Made some label name change in data file

--- App.R
+++ App.R
@@ -1,3 +1,63 @@
 library(shiny)
 library(sf)
 
+
+##Reading the data
+covid <- read.csv("covid-deaths.csv")
+tail(covid)
+
+##Writing ui
+
+ui <- fluidPage(
+  
+  ##Title of the application
+  titlePanel("Covid-Deaths in Canada during 2021"),
+  
+  ##Trying to make more pleasing using the themes
+  theme = bslib::bs_theme(bootswatch = "minty", version = 5),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      ##Sorting the data in filtered manner 
+      selectInput("region", label = "Select any Region:", choices = sort(unique(covid$REGIONS))),
+      selectInput("gender", label = "Select any Gender:", choices = sort(unique(covid$Gender))),
+      selectInput("age", label = "Select any age-group:", choices = sort(unique(covid$Age_group))),
+      
+      width = 3
+    ),
+    
+    ##Main panel for outputs 
+    mainPanel(
+      tabsetPanel(
+        
+        ##Panel 1
+        tabPanel(
+        "Data",
+        dataTableOutput("table")
+        )
+      ),
+      
+      width = 9
+    )
+    
+  )
+)
+
+
+##Writing server part 
+
+server <- function(input,output,session) {
+  
+  ##Table output
+  output$table <- renderDataTable({
+  })
+  
+}
+
+
+
+##Calling shiny application 
+
+shinyApp(ui,server)
